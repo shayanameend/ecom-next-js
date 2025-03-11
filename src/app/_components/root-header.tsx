ed9dd86f0a603979ca19463c46efb4a092a4b169
Shayan Ameen <shayan.ameen.developer@gmail.com>
2025-03-12 04:43:03 +0500

    feat: add Radix UI components for dialog and label, update RootHeader to use Sheet

--- src/app/_components/root-header.tsx
+++ src/app/_components/root-header.tsx
@@ -4,6 +4,16 @@ import { MenuIcon, ShoppingCartIcon } from "lucide-react";
 
 import { assets } from "~/assets";
 import { Button } from "~/components/ui/button";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "~/components/ui/sheet";
 import { cn } from "~/lib/utils";
 
 export function RootHeader() {
@@ -17,18 +27,32 @@ export function RootHeader() {
         priority
         src={assets.pictures.app.logo.src}
         alt={assets.pictures.app.logo.alt}
-        width={150}
         height={48}
         sizes="(max-width: 640px) 120px, (max-width: 1024px) 150px, 180px"
-        className={cn("h-12 w-auto")}
+        className={cn("h-12 w-auto object-cover")}
       />
       <div className={cn("flex flex-row items-center gap-4")}>
         <Button variant="outline" size="icon">
           <ShoppingCartIcon />
         </Button>
-        <Button variant="outline" size="icon">
-          <MenuIcon />
-        </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle />
+              <SheetDescription />
+            </SheetHeader>
+            <SheetFooter>
+              <SheetClose asChild>
+                <Button />
+              </SheetClose>
+            </SheetFooter>
+          </SheetContent>
+        </Sheet>
       </div>
     </header>
   );
